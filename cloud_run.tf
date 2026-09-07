0b319b2917fabe36a33c6fccfc434252983e4ea2
shahidmd78 <shahidm@gmail.com>
2026-09-07 11:32:42 -0400

    changes

--- cloud_run.tf
+++ cloud_run.tf
@@ -19,3 +19,12 @@ resource "google_cloud_run_v2_service" "app" {
     google_artifact_registry_repository.app
   ]
 }
+
+resource "google_cloud_run_v2_service_iam_member" "public_access" {
+  project  = var.gcp_project_id
+  location = var.region
+  name     = google_cloud_run_v2_service.app.name
+
+  role   = "roles/run.invoker"
+  member = "allUsers"
+}
